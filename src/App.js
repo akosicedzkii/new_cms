8035324f87dc0074938ab741ec901aa9bb05021a
Cederic Martinez <cedzkii81@gmail.com>
2022-05-09 10:59:27 +0800

    added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>Hello world! 2.0</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
